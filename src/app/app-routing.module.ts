94ee48aacda44aaea20cca289b8a424507b86dc5
ThamrinPH <husni.sosnet@gmail.com>
2020-04-30 12:40:33 +0700

    Revert "WIP totrial part 5"
    This reverts commit bbb825bfd6f851e6a4416b7b9bf332c358a3f6de.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent },
-];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
 })
 export class AppRoutingModule { }
